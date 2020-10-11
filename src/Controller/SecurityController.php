5260f550e6ed38032e04e3ad034bb1a25a200ae9
ismain <ismain.nabi12@gmail.com>
2020-10-12 01:12:05 +0200

    Add Register action

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Security\SecurityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -31,5 +35,30 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     *
+     * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param SecurityManager $securityManager
+     * @return Response
+     */
+    public function register(Request $request, SecurityManager $securityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $securityManager->register($user);
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé ! Vous pouvez maintenant vous connecter !"
+            );
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
