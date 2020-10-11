a390d41b434dada7b4c1520457dd2839fe43b1ac
ismain <ismain.nabi12@gmail.com>
2020-10-12 01:28:56 +0200

    Creation user fixtures for testing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $user = new User();
+        $user->setUsername('Hugues')
+            ->setEmail('Hugues@yopmail.com')
+            ->setPhone("0600000000")
+            ->setRoles(['ROLE_USER'])
+        ;
+        $password =  $this->encoder->encodePassword($user,'test');
+        $user->setPassword($password);
+        $manager->persist($user);
         $manager->flush();
     }
 }
